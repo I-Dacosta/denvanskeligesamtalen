262cc0828f141058275817f0a52657cf73180581
Ima Da costa <ima.dacosta@aquatiq.com>
2026-01-12 11:09:38 +0100

    Update Vercel Blob storage config to fix importMap issue

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -36,7 +36,9 @@ export default buildConfig({
     vercelBlobStorage({
       enabled: true,
       collections: {
-        media: true,
+        media: {
+          disablePayloadAccessControl: true,
+        },
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || "",
     }),
